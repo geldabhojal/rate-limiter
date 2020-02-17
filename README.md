c7ab766c0299991d5032a06f9a1d70a6f160d6a9
Bhojal Gelda <bgelda@us.ibm.com>
2020-02-17 12:53:44 -0600

    initial commit for working POC of rate limiting in distributed applications using zookeeper

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # rate-limiter
-Distributed Rate Limiting backed by Zookeeper
+Rate Limiting in Distributed Applications using Zookeeper
+
+In a distributed environment, the state needs to be mutated by different applications running in different processes and still be able to observe the same state across them. Zookeeper like distributed stores which provide with locking mechanism seems like a very good candidate for this. The way zookeeper provides locking is through a lock path, where every client trying to aquire lock comes and adds an EPHEMERAL node in that path. Zookeeper maintains a priority queue of the clients arriving in order and provides lock in that order.
+
+In our use case, we want to rate limit our user traffic over a certain period of time. The allowed rate(bandwidth) of the user gets refresh after every configured period called the RefreshWindow. Each individual rate limit node is able to update/see the same bandwidth utilization across using locks.
+
+Algorithm and Design Decisions
+Input: RequestSize, Error Channel
+Output: error if the user has exhausted it's bandwidth the time window else nil
+
+#Procedure
+
+1) Create and set user specific quota paths in zookeeper.
+```
+/quota/test-user/total 100
+/quota/test-user/base 10
+/quota/test-user/usable 100
+/quota/test-user/refresh
+```
+
+2) Watch the above paths
+3) Store Mtime(modified time) of the /refresh node so that it can be checked when is the refresh required
+4) Start a thread which compares current with the Mtime of the refresh node to decide if the refresh is required.
+ If refreshWindow is hit
+           - Aquire Zookeeper Lock
+           - Set Mtime on the refresh zk node to notify other rate-limit applications that a refresh is in progress. That way they don't need to refresh in case they also hit the refresh window at the same time
+           - Update the /usable with /total value to refresh the quota back to full
+           - Check every 'x' amount of time if the refresh is required, where 'x' completely divides the refresh window
+
+5) Start a thread to mimic(for POC) requests coming with randomized size and the VerifyQuota using the following business logic.
+```
+// if no usable quota left for this window, this will not let any inconsistent response to the customer
+    // because when any node marks the usable quota for that window 0 , all will see it.
+    // this means to provide user with it's assigned quota, we must start with an extra base quota on start up
+    if rl.usableQuotaLeft <= 0 {
+        response <- errors.New("no usable quota for this refreshWindow, waiting for it to be refreshed")
+        return
+    }
+ 
+    rl.quotaLeft = rl.quotaLeft - int64(reqSize)
+ 
+    // if local quota left is 20% of the base quota, request more parallely for optimization
+    if rl.quotaLeft <= int64(rl.baseQuota*20/100) && !rl.optimizing {
+        go rl.addBaseQuota()
+    }
+ 
+    if rl.quotaLeft >= 0 {
+        log.Println("user has bandwidth to pass through request with size  ", int64(reqSize), " local quota left ", rl.quotaLeft)
+        response <- nil
+        return
+    } else {
+        log.Println("base quota not enough for the request size, requesting more..")
+        // request more local quota from zookeeper
+        rl.requestQuota(reqSize, response)
+    }
+```
+
+6)  update new usable quota in zookeeper with the following
+```
+if zk has sufficient usable quota {
+	// take quota for itself
+	// eg. newUsableQuota := usableQuotaLeft(70) - reqSize(15) - baseQuota(10) = 45
+	newUsableQuota := usableQuotaLeft - reqSize - baseQuota
+	// update zookeeper with new quota
+} else {
+       update usable quota with 0
+}
+```
+
+#Optimizations
+
+1) Because we would not want to add latency to the requests coming in, one thing that can be done is requests flowing through and quota being calculated parallely, that way even though some amount of data goes through, the next requests will fail with quota exhaustion. The response from the rate limit can be update in a HashMap which is {"test-user": 0}, where 0 meaning further requests cannot be sent in this time window. 
+This is something that can be decided in upstream.
+
+2) Because every time local base quota gets over for a rate limit node, it has to go to zookeeper to ask for more before it can let the request go further, I added logic to ask for more quota concurrently, without affecting the traffic, every time 80% of the local quota is consumed and only 20% is left, that way the local quota keeps getting refilled concurrently and the requests don't wait.
+
+A possible situation in this could be that, the requests for a specific user is skewed to one rate-limit node and it asks for more quota but suddenly it gets skewed to the other node, a small amount of quota gets wasted at that point of time that the previous node asked for. But this should not be a major concern because 
+    a) we always start with an extra overall total quota than promised to the user.
+    b) Relinquish excess quota that the rate-limit node might have every 15seconds back to zookeeper. In other words, if somehow(highly unlikely in my algorithm) quota gets concentrated in one node such that it is more than twice the size of our base quota. Again, we started with number of app-nodes * baseQuota as extra quota in the beginning, so loosing 2*baseQuota is not a quota leak.
+
+3) Our base quota should be small enough to avoid leaks threw skewness. Optimization 2 can also help in reducing the number of hits to zookeeper when small base quota because we ask for more quota parallely when the base quota is about to finish.
+
+
---
+++ rate-limiter.go
@@ -0,0 +1,428 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/curator-go/curator"
+	recipe "github.com/curator-go/curator/recipes"
+	zk "github.com/samuel/go-zookeeper/zk"
+)
+
+const (
+	prefix        = "/quota"
+	refreshSuffix = "/refresh"
+	baseSuffix    = "/base"
+	totalSuffix   = "/total"
+	usableSuffix  = "/usable"
+	// concatenate username to make a lockPath
+	lockPath = "/lock-"
+)
+
+var (
+	QUOTA_EXHAUSTED = errors.New("quota exhausted for time period message, waiting to be refreshed")
+)
+
+type RateLimitService interface {
+	VerifyQuota(int64, chan error)
+}
+
+// RateLimit rate limit a user
+type RateLimit struct {
+	username          string
+	usableQuotaPath   string
+	baseQuotaPath     string
+	totalQuotaPath    string
+	refreshQuotaPath  string
+	quotaLock         sync.Mutex
+	m                 sync.Mutex
+	lockTimeout       time.Duration
+	refreshWindow     time.Duration
+	baseQuota         int64
+	totalAllowedQuota int64
+	usableQuotaLeft   int64
+	cTime             int64
+	mTime             int64
+	quotaLeft         int64
+	lock              *recipe.InterProcessMutex
+	client            curator.CuratorFramework
+	optimizing        bool
+}
+
+func NewRateLimit(client curator.CuratorFramework, username string,
+	totalAllowedQuota, baseQuota int64, lockTimeout time.Duration, refreshWindow time.Duration) *RateLimit {
+	var err error
+	rl := &RateLimit{
+		username:          username,
+		totalAllowedQuota: totalAllowedQuota,
+		usableQuotaLeft:   totalAllowedQuota,
+		baseQuota:         baseQuota,
+		lockTimeout:       lockTimeout,
+		refreshWindow:     refreshWindow,
+		client:            client,
+		baseQuotaPath:     prefix + "/" + username + baseSuffix,
+		usableQuotaPath:   prefix + "/" + username + usableSuffix,
+		totalQuotaPath:    prefix + "/" + username + totalSuffix,
+		refreshQuotaPath:  prefix + "/" + username + refreshSuffix,
+	}
+
+	// initialize the lock to be used and inject it whereever required.
+	rl.lock, err = recipe.NewInterProcessMutex(rl.client, lockPath+username)
+	if err != nil {
+		log.Fatalf("distributed lock to zookeeper could not be initialized", err)
+	}
+
+	rl.create(prefix, []byte(""))
+	rl.create(prefix+"/"+rl.username, []byte(""))
+
+	rl.create(rl.baseQuotaPath, []byte(strconv.FormatInt(rl.baseQuota, 10)))
+
+	rl.create(rl.totalQuotaPath, []byte(strconv.FormatInt(rl.totalAllowedQuota, 10)))
+
+	rl.create(rl.usableQuotaPath, []byte(strconv.FormatInt(rl.usableQuotaLeft, 10)))
+
+	rl.create(rl.refreshQuotaPath, []byte(""))
+
+	rl.addWatch()
+
+	return rl
+}
+
+func (rl *RateLimit) addWatch() {
+	var err error
+	rl.baseQuota, err = strconv.ParseInt(string(rl.watch(rl.baseQuotaPath, rl.baseQuotaEvent)), 10, 64)
+	if err != nil {
+		log.Println(err)
+	}
+	// get and watch the overall usable quota for the user that he is consuming from
+	rl.usableQuotaLeft, err = strconv.ParseInt(string(rl.watch(rl.usableQuotaPath, rl.usableQuotaEvent)), 10, 64)
+	if err != nil {
+		log.Println(err)
+	}
+	// get and watch the configured totalAllowedQuota
+	rl.totalAllowedQuota, err = strconv.ParseInt(string(rl.watch(rl.totalQuotaPath, rl.totalQuotaEvent)), 10, 64)
+	if err != nil {
+		log.Println(err)
+	}
+	rl.quotaLeft = int64(rl.baseQuota)
+	// check for last refreshed  and createTime fo refresh node to use for refresh time calculations
+	rl.mTime, rl.cTime = rl.watchRefreshNode(rl.refreshQuotaPath, rl.refreshQuotaEvent)
+	log.Println("last refresh happened ", time.Now().Unix()-rl.mTime, "seconds ago")
+
+	// concurrently look to refresh quota
+	go rl.refreshQuota()
+	// mimic user requests being processed with random size
+	go rl.startRequests()
+	// just in case there is skewness observed through loadbalancer and
+	// quota gets concentrated on a single rate limit node
+	go rl.relinquish()
+}
+
+func (rl *RateLimit) create(path string, data []byte) error {
+	var err error
+	stat, err := rl.client.CheckExists().ForPath(path)
+	if stat == nil {
+		_, err = rl.client.Create().ForPathWithData(path, data)
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	retryPolicy := curator.NewExponentialBackoffRetry(time.Second, 3, 15*time.Second)
+	client := curator.NewClient("localhost:2181", retryPolicy)
+	client.Start()
+	defer client.Close()
+	// where to close zookeeper connection
+	//defer rl.client.Stop()
+	_ = NewRateLimit(client, "bhojal", 100000, 100, 5*time.Second, 60*time.Second)
+	//100/m rate
+
+	shutdownChannel := make(chan os.Signal, 2)
+	signal.Notify(shutdownChannel, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(<-shutdownChannel)
+}
+
+// this function mimics generating requests
+func (rl *RateLimit) startRequests() {
+	for {
+		// start a user request every 1 second
+		go func() {
+			size := rl.requestSize()
+			err := make(chan error)
+			go rl.VerifyQuota(size, err)
+			if e := <-err; e != nil {
+				// respond to the user about quota exhaustion
+				log.Println(e)
+			} else {
+				log.Println("request OK to be sent downstream")
+			}
+			return
+		}()
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func (rl *RateLimit) requestSize() int64 {
+	var size int
+	for {
+		s1 := rand.NewSource(time.Now().UnixNano())
+		r1 := rand.New(s1)
+		size = r1.Intn(15)
+		// avoiding a request of size 0 to be returned
+		if size != 0 {
+			break
+		}
+	}
+	return int64(size)
+}
+
+func (rl *RateLimit) refreshQuota() {
+	for {
+		now := time.Now().Unix()
+		// Note: Because we Aquire the lock to refresh quota, we should keep refreshWindow to a reasonable value and not like every few seconds
+		if now-rl.mTime >= int64(rl.refreshWindow.Seconds()) {
+			ok, err := rl.lock.Acquire()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			defer rl.lock.Release()
+			if ok {
+				// refresh only if the usableQuotaLeft is <=total allowed quota
+				// check for time.Now().Unix()-mTime again incase another node just refreshed
+				if rl.usableQuotaLeft <= rl.totalAllowedQuota && time.Now().Unix()-rl.mTime >= int64(rl.refreshWindow.Seconds()) {
+					// set anything on the refresh node to update it's mTime
+					err := rl.set(rl.refreshQuotaPath, []byte(""))
+					if err != nil {
+						log.Println("resetting quota failed ", err)
+						return
+					}
+					// refresh the quota with totalQuota for this new window.
+					err = rl.set(rl.usableQuotaPath, []byte(strconv.FormatInt(rl.totalAllowedQuota, 10)))
+					if err != nil {
+						log.Println("refreshing quota failed ", err)
+						return
+					} else {
+						rl.lock.Release()
+						log.Println("QUOTA REFRESHED")
+					}
+				} else if rl.usableQuotaLeft > rl.totalAllowedQuota {
+					rl.lock.Release()
+					// we decide to make the system self correct itself by setting /quotas/usable equals to /quotas/total
+					log.Println("WARN: usable quota is more than total allowed , an impossible case unless somebody manually changed the 'quota/usable' in zookeeper")
+				} else {
+					// some other rate-limit node refreshed or this is the first time the application is starting up
+					log.Println("the quota was recently refreshed, need not be refreshed")
+					rl.lock.Release()
+				}
+			}
+		}
+		//For eg. if 100MB/30min is the limit, it makes sense check every 10 min for refreshment
+		// 30min/60 *2 = 600sec = 10min
+		time.Sleep(time.Duration(rl.refreshWindow.Seconds()/60) * 2 * time.Second)
+	}
+}
+
+// GetQuota take the size of the request and calculates usable bandwidth
+// we should be using a channel for the user request to wait on GetQuota's
+// response which is if the user has exhausted it's quota or not
+func (rl *RateLimit) VerifyQuota(reqSize int64, response chan error) {
+	rl.m.Lock()
+	defer rl.m.Unlock()
+	log.Println("request size received", reqSize)
+
+	// if no usable quota left for this window, this will not let any inconsistent response to the customer
+	// because when any node marks the usable quota for that window 0 , all will see it.
+	// this means to provide user with it's assigned quota, we must start with an extra base quota on start up
+	if rl.usableQuotaLeft <= 0 {
+		response <- errors.New("no usable quota for this refreshWindow, waiting for it to be refreshed")
+		return
+	}
+
+	rl.quotaLeft = rl.quotaLeft - int64(reqSize)
+
+	// if local quota left is 20% of the base quota, request more parallely for optimization
+	if rl.quotaLeft <= int64(rl.baseQuota*20/100) && !rl.optimizing {
+		go rl.addBaseQuota()
+	}
+
+	if rl.quotaLeft >= 0 {
+		log.Println("user has bandwidth to pass through request with size  ", int64(reqSize), " local quota left ", rl.quotaLeft)
+		response <- nil
+		return
+	} else {
+		log.Println("base quota not enough for the request size, requesting more..")
+		// request more local quota from zookeeper
+		rl.requestQuota(reqSize, response)
+	}
+}
+
+func (rl *RateLimit) requestQuota(reqSize int64, response chan error) {
+	ok, err := rl.lock.Acquire()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer rl.lock.Release()
+	if ok {
+		// this rate-limit node has used up it's base quota or may be usableQuotaLeft
+		if reqSize > rl.usableQuotaLeft {
+			response <- QUOTA_EXHAUSTED
+		} else {
+			// let the request go though since user has usable bandwidth for refreshWindow
+			response <- nil
+		}
+		log.Println("overall usable quota known is", rl.usableQuotaLeft)
+		// nupdate usableQuota in store with the new reduced value
+		if rl.usableQuotaLeft-rl.baseQuota > 0 && rl.usableQuotaLeft-reqSize >= rl.baseQuota {
+			// take quota for itself
+			rl.quotaLock.Lock()
+			rl.quotaLeft = int64(rl.baseQuota)
+			rl.quotaLock.Unlock()
+			newUsableQuota := rl.usableQuotaLeft - reqSize - rl.quotaLeft
+			err := rl.set(rl.usableQuotaPath, []byte(strconv.FormatInt(newUsableQuota, 10)))
+			if err != nil {
+				log.Println("updating usable quota failed ", err)
+				return
+			}
+			log.Println("took ", rl.baseQuota, " from overall usable quota, updating usable quota to store:", newUsableQuota)
+			return
+		} else {
+			// update usableQuota with 0 since user does not have usable quota and wait for the refreshWindow
+			err := rl.set(rl.usableQuotaPath, []byte(strconv.FormatInt(0, 10)))
+			if err != nil {
+				log.Println("updating usable quota failed ", err)
+				return
+			}
+			log.Println("last quota bucket did not have enought, waiting for refresh")
+		}
+	}
+}
+
+// just in case there is skewness observed through loadbalancer and
+// quota gets concentrated on a single rate limit node
+func (rl *RateLimit) relinquish() {
+	for {
+		if rl.quotaLeft > 2*rl.baseQuota {
+			ok, err := rl.lock.Acquire()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			defer rl.lock.Release()
+			if ok {
+				rl.quotaLock.Lock()
+				rl.usableQuotaLeft = rl.usableQuotaLeft + rl.quotaLeft - rl.baseQuota
+				rl.quotaLeft = rl.baseQuota
+				log.Println("had too much quota, relinquising ", rl.quotaLeft-rl.baseQuota)
+				err := rl.set(rl.usableQuotaPath, []byte(strconv.FormatInt(rl.usableQuotaLeft, 10)))
+				if err != nil {
+					log.Println("updating usable quota failed ", err)
+					return
+				}
+			}
+		}
+		time.Sleep(15 * time.Second)
+	}
+}
+
+func (rl *RateLimit) addBaseQuota() {
+	rl.optimizing = true
+	defer func() {
+		rl.optimizing = false
+	}()
+	ok, err := rl.lock.Acquire()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer rl.lock.Release()
+	if ok {
+		log.Println("optimizing by pulling 20% base quota")
+		if rl.usableQuotaLeft >= rl.baseQuota {
+			rl.quotaLock.Lock()
+			rl.quotaLeft = int64(float64(rl.quotaLeft) + float64(0.4)*float64(rl.baseQuota))
+			rl.quotaLock.Unlock()
+			log.Println("new local quota ", rl.quotaLeft)
+			newUsableQuota := int64(float64(rl.usableQuotaLeft) - float64(0.4)*float64(rl.baseQuota))
+			err := rl.set(rl.usableQuotaPath, []byte(strconv.FormatInt(newUsableQuota, 10)))
+			if err != nil {
+				log.Println("updating usable quota failed ", err)
+				return
+			}
+			log.Println("optimization:took ", int64(float64(0.4)*float64(rl.baseQuota)), " from overall usable quota, updating usable quota to store:", newUsableQuota)
+		} else {
+			log.Println("not enough usable quota left for optimization")
+		}
+	}
+}
+
+func (rl *RateLimit) set(path string, data []byte) error {
+	_, err := rl.client.SetData().ForPathWithData(path, data)
+	return err
+}
+
+func (rl *RateLimit) watch(path string, callback func(*zk.Event)) []byte {
+	b, err := rl.client.GetData().UsingWatcher(curator.NewWatcher(callback)).ForPath(path)
+	if err != nil {
+		log.Fatal("watcher failed ", err)
+	}
+	q, err := strconv.ParseInt(string(b), 10, 64)
+	log.Println(path, " quota for all goroutines was updated as ", q)
+	return b
+}
+
+func (rl *RateLimit) watchRefreshNode(path string, callback func(*zk.Event)) (int64, int64) {
+	stat, err := rl.client.CheckExists().UsingWatcher(curator.NewWatcher(callback)).ForPath(path)
+	if err != nil {
+		log.Fatalf("check exists failed", err)
+	}
+	if stat == nil {
+		log.Fatal("Fatal: refresh node does not exist in zookeeper")
+	}
+	return stat.Mtime / 1000, stat.Ctime / 1000
+}
+
+func (rl *RateLimit) usableQuotaEvent(event *zk.Event) {
+	rl.quotaLock.Lock()
+	defer rl.quotaLock.Unlock()
+	var err error
+	rl.usableQuotaLeft, err = strconv.ParseInt(string(rl.watch(rl.usableQuotaPath, rl.usableQuotaEvent)), 10, 64)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (rl *RateLimit) totalQuotaEvent(event *zk.Event) {
+	rl.quotaLock.Lock()
+	defer rl.quotaLock.Unlock()
+	var err error
+	rl.totalAllowedQuota, err = strconv.ParseInt(string(rl.watch(rl.totalQuotaPath, rl.totalQuotaEvent)), 10, 64)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (rl *RateLimit) baseQuotaEvent(event *zk.Event) {
+	rl.quotaLock.Lock()
+	defer rl.quotaLock.Unlock()
+	var err error
+	rl.baseQuota, err = strconv.ParseInt(string(rl.watch(rl.baseQuotaPath, rl.baseQuotaEvent)), 10, 64)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (rl *RateLimit) refreshQuotaEvent(event *zk.Event) {
+	rl.mTime, rl.cTime = rl.watchRefreshNode(rl.refreshQuotaPath, rl.refreshQuotaEvent)
+}
